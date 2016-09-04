0ca7f61e5e1be50c11766ba3de8df198090e130b
mayur vijay kode <mayurkode7@gmail.com>
2016-09-04 15:31:09 +0530

    Update app.js

--- ArtistDirectory/scripts/app.js
+++ ArtistDirectory/scripts/app.js
@@ -1,3 +1,5 @@
+
+window.scrollTo(0, 1);
 var myApp = angular.module("artistsDirectory",[
                                                 "ngRoute",
                                                 "artistsControllers"
